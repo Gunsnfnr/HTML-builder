f24044ea2679df7a4255558b6f7969c38518b597
Gunsnfnr <a.j.p.010203@gmail.com>
2024-01-19 23:10:36 +0300

    feat: add task 2 solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'new.txt'));
+rl.question('Hello! How are you today? ', (answer) => {
+  writeStream.write(answer);
+  rl.on('line', (answer) => {
+    if (answer.match(/^exit$/i)) {
+      rl.close();
+    } else {
+      writeStream.write('\n' + answer);
+    }
+  });
+  rl.on('SIGINT', () => rl.close());
+});
+
+rl.on('close', () => {
+  console.log('Writing to file completed');
+});
